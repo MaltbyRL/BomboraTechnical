00be576779e2e22eb2a1e0f6272936c1c4617612
MaltbyRL <RLMaltby@gmail.com>
2016-03-27 14:47:17 -0700

    MVP met, refactoring still needed

--- app.js
+++ app.js
@@ -1,18 +1,113 @@
 "use strict";
 const jsonexport = require('jsonexport')
 const fs = require('fs')
+let xmlData;
 
-let xmlData = `<school id="100"><grade id="1"><classroom id="101" name="Mrs. Jones' Math Class"><teacher id="10100000001" first_name="Barbara" last_name="Jones"/><student id="10100000010" first_name="Michael" last_name="Gil"/><student id="10100000011" first_name="Kimberly" last_name="Gutierrez"/><student id="10100000013" first_name="Toby" last_name="Mercado"/><student id="10100000014" first_name="Lizzie" last_name="Garcia"/><student id="10100000015" first_name="Alex" last_name="Cruz"/></classroom><classroom id="102" name="Mr. Smith's PhysEd Class"><teacher id="10200000001" first_name="Arthur" last_name="Smith"/><teacher id="10200000011" first_name="John" last_name="Patterson"/><student id="10200000010" first_name="Nathaniel" last_name="Smith"/><student id="10200000011" first_name="Brandon" last_name="McCrancy"/><student id="10200000012" first_name="Elizabeth" last_name="Marco"/><student id="10200000013" first_name="Erica" last_name="Lanni"/><student id="10200000014" first_name="Michael" last_name="Flores"/><student id="10200000015" first_name="Jasmin" last_name="Hill"/><student id="10200000016" first_name="Brittany" last_name="Perez"/><student id="10200000017" first_name="William" last_name="Hiram"/><student id="10200000018" first_name="Alexis" last_name="Reginald"/><student id="10200000019" first_name="Matthew" last_name="Gayle"/></classroom><classroom id="103" name="Brian's Homeroom"><teacher id="10300000001" first_name="Brian" last_name="O'Donnell"/></classroom></grade</school>`
-let cleanedDataArray = []
+const consolidateStudentObjects = (data) => {
+  let arrayOfFullStudentObjects = [];
+  let school, grade, classroom, teacherOne, teacherTwo;
+  let studentObject = {};
+  let counter = 0;
+  let addTeacher = true;
+  data.forEach(function(object, i) {
+    if (object.hasOwnProperty("school_id")) {
+      school = {}
+      school["school_id"] = object["school_id"]
+    } else if (object.hasOwnProperty("grade_id")) {
+      grade = {}
+      grade["grade_id"] = object["grade_id"]
 
+    } else if (object.hasOwnProperty("classroom_id")) {
+      classroom = {};
+      addTeacher = true;
 
+      classroom["classroom_id"] = object["classroom_id"]
+      classroom["classroom_name"] = object["classroom_name"]
+      if (studentObject.hasOwnProperty("2nd_teacher_id")) {
+        console.log("hit")
+        teacherTwo = {};
+        teacherTwo["2nd_teacher_id"] = "";
+        teacherTwo["2nd_teacher_first_name"] = "";
+        teacherTwo["2nd_teacher_last_name"] = "";
+      }
+    } else if (object.hasOwnProperty("teacher_id")) {
+      if (addTeacher) {
+        teacherOne = {}
+        teacherTwo = {}
+        counter++
+        teacherOne["teacher_id"] = object["teacher_id"]
+        teacherOne["teacher_first_name"] = object["teacher_first_name"]
+        teacherOne["teacher_last_name"] = object["teacher_last_name"]
+        teacherTwo["2nd_teacher_id"] = "";
+        teacherTwo["2nd_teacher_first_name"] = "";
+        teacherTwo["2nd_teacher_last_name"] = "";
+        addTeacher = false;
+      } else {
+        teacherTwo = {}
+        teacherTwo["2nd_teacher_id"] = object["teacher_id"]
+        teacherTwo["2nd_teacher_first_name"] = object["teacher_first_name"]
+        teacherTwo["2nd_teacher_last_name"] = object["teacher_last_name"]
+      }
+    }
+    if (object.hasOwnProperty("student_id") || counter === 3){
 
+      studentObject = {}
+      studentObject["grade_id"] = grade["grade_id"]
+      studentObject["classroom_id"] = classroom["classroom_id"]
+      studentObject["classroom_name"] = classroom["classroom_name"]
+      studentObject["teacher_2_id"] = teacherTwo["2nd_teacher_id"]
+      studentObject["teacher_2_first_name"] = teacherTwo["2nd_teacher_first_name"]
+      studentObject["teacher_2_last_name"] = teacherTwo["2nd_teacher_last_name"]
+      studentObject["teacher_id"] = teacherOne["teacher_id"]
+      studentObject["teacher_first_name"] = teacherOne["teacher_first_name"]
+      studentObject["teacher_last_name"] = teacherOne["teacher_last_name"]
+      studentObject["school_id"] = school["school_id"]
+      studentObject["student_id"] = object["student_id"]
+      studentObject["student_first_name"] = object["student_first_name"]
+      studentObject["student_last_name"] = object["student_last_name"]
+      console.log(">>>",studentObject)
+      arrayOfFullStudentObjects.push(studentObject)
+    }
+  })
+  console.log("<<<<<", arrayOfFullStudentObjects)
+  // console.log(">>>>", arrayOfFullStudentObjects)
+  return arrayOfFullStudentObjects
+}
 
+// Input: [ [ { school_id: '100"' } ],[ { grade_id: '1"' } ], [ { classroom_id: '101"' },{ classroom_name: 'Mrs_Jones_Math Class"' } ]]
+// Output: [ { school_id: '100"' }, { grade_id: '1"' }, { classroom_id: '101"', classroom_name: 'Mrs_Jones_Math Class"' }]
+const flattenArrayOfObjects = (data) => {
+  let unnestedArray = []
+  data.map(function(data) {
+    if (Array.isArray(data)) {
+      unnestedArray = unnestedArray.concat(flatten(data));
+    } else {
+      unnestedArray.push(data);
+    }
+  })
+  return unnestedArray
+}
+
+// Input:
+// Output:
+const organizeArrayObject = (data) => {
+  let arrayOfObjects = []
+  data.forEach(function(part) {
+    let buildObject = {}
+    part.forEach(function(object) {
+      let objectKey = Object.keys(object)[0]
+      let objectValue = object[objectKey]
+      buildObject[objectKey] = objectValue
+    })
+    arrayOfObjects.push(buildObject)
+  })
+  return arrayOfObjects
+}
 
 // Input: <school id="100"/><grade id="1"/><classroom id="101" name="Mrs_Jones_Math Class"/><teacher id="10100000001" first_name="Barbara" last_name="Jones"/>
 // Output: [ [ { school_id: '100"' } ], [ { grade_id: '1"' } ], [ { classroom_id: '101"' }, { classroom_name: 'Mrs_Jones_Math Class"' } ]]
-
 const cleanXml = (data) => {
+  let cleanedDataArray = []
   let makeObject = data.match(/<[a-zA-Z]+((.(?!<\/)(?!<[a-zA-Z]+))*)?>/g);
   for (var i = 0; i < makeObject.length; i++) {
     let arrayOfTagData = []
@@ -33,61 +128,10 @@ const cleanXml = (data) => {
   return cleanedDataArray
 }
 
+// Removes Doctype, ensures no edge cases in strings.
+// Outputs uniform data to easily work with.
+const removeEdgeCases = (data) => {
 
-//////////////////// Input
-//[ [ { school_id: '100"' } ], [ { grade_id: '1"' } ], [ { classroom_id: '101"' }, { classroom_name: 'Mrs_Jones_Math Class"' } ]]
-////////////////// Output
-
-const organizeArrayObject = (data) => {
-  let arrayOfObjects = []
-
-  data.forEach(function(part) {
-    let buildObject = {}
-    part.forEach(function(object) {
-      let objectKey = Object.keys(object)[0]
-      let objectValue = object[objectKey]
-      buildObject[objectKey] = objectValue
-    })
-    arrayOfObjects.push(buildObject)
-
-
-  })
-  return arrayOfObjects
-}
-
-
-// input: [ [ { school_id: '100"' } ],[ { grade_id: '1"' } ], [ { classroom_id: '101"' },{ classroom_name: 'Mrs_Jones_Math Class"' } ]]
-//output: [ { school_id: '100"' }, { grade_id: '1"' }, { classroom_id: '101"', classroom_name: 'Mrs_Jones_Math Class"' }]
-
-const flattenArrayOfObjects = (data) => {
-  let unnestedArray = []
-  data.map(function(data) {
-    if (Array.isArray(data)) {
-      unnestedArray = unnestedArray.concat(flatten(data));
-    } else {
-      unnestedArray.push(data);
-    }
-  })
-  console.log(unnestedArray)
-}
-
-const consolidateStudentObjects = (data) => {
-
-}
-
-//runs data through necessary steps.
-
-const fixTagsOf = (data) => {
-  data = cleanXml(data)
-  data = organizeArrayObject(data)
-  data = flattenArrayOfObjects(data)
-  data = consolidateStudentObjects(data)
-  return data
-}
-
-//removes Doctype, ensures no edge cases in strings.
-//outputs uniform data to easily work with.
-const cleanThe = (data) => {
   data = data.replace(/('s\s)/g, "s_")
     .replace(/\<\?xml.+\?\>|\<\!DOCTYPE.+]\>/g, "")
     .replace(/\.\s/g, "_")
@@ -96,27 +140,33 @@ const cleanThe = (data) => {
     .replace(/\s+</g, "<")
     .replace(/\"\>/g, '"/>')
     .replace(/>\s+/g, ">")
-  data = fixTagsOf(data)
   return data
 }
 
-
+// Runs data through necessary steps.
 const convertXml = (data) => {
-  data = cleanThe(data)
-    // console.log(data)
+  data = removeEdgeCases(data)
+  data = cleanXml(data)
+  data = organizeArrayObject(data)
+  data = flattenArrayOfObjects(data)
+  data = consolidateStudentObjects(data)
   return data
-};
+}
 
+fs.readFile('./xml-to-be-converted/sample_data.xml', function read(err, data) {
+  if (err) {
+    console.log("err")
+    throw err;
+  }
+  xmlData = data.toString('utf8');
+  console.log(xmlData)
 
-
-
-
-
-jsonexport(convertXml(xmlData), (err, csv) => {
-  if (err) console.log("err:", err)
-    // console.log(csv)
-    // fs.writeFile('newCSV.csv', csv, function(err, file){
-    //     console.log(err, file);
-    //   });
-  console.log("nothing Broken")
-})
+  jsonexport(convertXml(xmlData), (err, csv) => {
+    if (err) console.log("err:", err)
+    console.log(csv)
+    fs.writeFile('converted-to-csv/RichardsCSV.csv', csv, function(err, file) {
+      console.log(err, file);
+    });
+    console.log("nothing Broken")
+  })
+});
